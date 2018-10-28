0f94a9394d78d7db0b62f4e9f096966dd6c44865
Soumyaranjan Mohanty <soumya8018@gmail.com>
2018-10-28 20:26:46 +0530

    Created the tables in calender and update the todayCheck(day) method and createCalender method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit, ViewEncapsulation } from '@angular/core';
+import * as moment from 'moment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'synoriq-date-time-picker';
+   public date = moment();
+  public daysArr;
+
+  constructor() {
+   
+  }
+  public ngOnInit() {
+    this.daysArr = this.createCalendar(this.date);
+  }
+  //To show the dates in the table using moment
+  public createCalendar(month) {
+    let firstDay = moment(month).startOf('M');
+    let days = Array.apply(null, { length: month.daysInMonth() })
+      .map(Number.call, Number)
+      .map(n => {
+        return moment(firstDay).add(n, 'd');
+      });
+    for(let n=0;n<firstDay.weekday();n++)
+    {
+    	days.unshift(null);
+    }
+    return days;
+  }
+  //Current date in red colour
+  public todayCheck(day) {
+    if (!day) {
+      return false;
+    }
+    return moment().format('L') === day.format('L');
+  }
 }
